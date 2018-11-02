0f6e992c39ade9341e438bf0726906ae2dfd7bac
SebastianSamayoa <samayoa_jhoan@hotmail.com>
2018-11-02 12:52:37 -0600

    moviendo los servicios

--- src/test/java/com/configserver/ConfiguracionesServidor/ConfiguracionesServidorApplicationTests.java
+++ src/test/java/com/configserver/ConfiguracionesServidor/ConfiguracionesServidorApplicationTests.java
@@ -3,10 +3,12 @@ package com.configserver.ConfiguracionesServidor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableConfigServer
 public class ConfiguracionesServidorApplicationTests {
 
 	@Test
